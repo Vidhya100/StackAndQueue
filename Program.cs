f5e0b0d13c63c23cb48600d4d41e3bb70c6c4bff
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-12 18:28:48 +0530

    stack operations using linked list

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace StackAndQueue
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedListStack list = new LinkedListStack();
+
+            list.Push(56);
+            list.Push(30);
+            list.Push(70);
+                
+            list.Display();
+
+            while (!list.IsEmpty())
+            {
+                list.Peek();
+                list.Pop();
+            }
+         }
+    }
+}
